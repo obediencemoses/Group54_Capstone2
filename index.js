96ce322954be61a14062323ddf1e2524b45e0965
obediencemoses <obediencepraise@gmail.com>
2021-11-25 23:26:08 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const m_Nav = document.getElementsByClassName("m-nav");
 
 function hamclick() {
     alert(m_Nav)
+    alert(m_Nav.classList.length)
+    document.getElementsByClassName("m-nav").classList.add("active");
 }
 
 
