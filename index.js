fb168d611f7f60c805e9bee04c95e3672e980528
obediencemoses <83711345+obediencemoses@users.noreply.github.com>
2021-11-26 00:41:12 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const m_Nav = document.getElementsByClassName("m-nav");
 
 function hamclick() {
-    alert(m_Nav)
     alert(m_Nav.classList.length)
     document.getElementsByClassName("m-nav").classList.add("active");
 }
